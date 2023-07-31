1708251fdff94d35c78abad82952b55e4724e8e6
Vaibhav <vaibhav.shukla98871174@gmail.com>
2023-07-31 15:07:08 +0530

    add a throttling example

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 
 	var button = document.getElementById("debounce");
-	const debounce = (func, delay) => {
-		let debounceTimer
-		return function() {
-				clearTimeout(debounceTimer)
-					debounceTimer
-				= setTimeout(() => func(), delay)
-		}
-	}
-	button.addEventListener('click', debounce(function() {
-			alert("Hello\nNo matter how many times you" +
-				"click the debounce button, I get " +
-				"executed once every 3 seconds!!")
-							}, 3000));
+	// const debounce = (func, delay) => {
+	// 	let debounceTimer
+	// 	return function() {
+	// 			clearTimeout(debounceTimer)
+	// 				debounceTimer
+	// 			= setTimeout(() => func(), delay)
+	// 	}
+	// }
+	// button.addEventListener('click', debounce(function() {
+	// 		alert("Hello\nNo matter how many times you" +
+	// 			"click the debounce button, I get " +
+	// 			"executed once every 3 seconds!!")
+	// 						}, 3000));
+
+    button.addEventListener('click',throttleFunction(()=>{
+        console.log('clicked')
+    },1500))
+
+    function throttleFunction(func,delay){
+        let prev = 0;
+
+        return ()=>{
+            let now = new Date().getTime()
+
+            console.log(now-prev,delay)
+
+            if(now-prev > delay){
+                prev = now;
+                return func();
+            }
+        }
+    }
